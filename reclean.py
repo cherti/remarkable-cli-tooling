0ebb107eae798022c71fc6ab65c30223b7630a56
Masataro Asai <guicho2.71828@gmail.com>
2022-07-25 19:02:19 -0400

    automatically terminate on error: reclean

--- reclean.py
+++ reclean.py
@@ -29,21 +29,11 @@ args = parser.parse_args()
 
 ssh_command = f'ssh -o PubkeyAcceptedKeyTypes=+ssh-rsa -o HostKeyAlgorithms=+ssh-rsa -S {ssh_socketfile} root@{args.ssh_destination}'
 
-ssh_connection = subprocess.Popen(f'{ssh_command} -o ConnectTimeout=1 -M -N -q ', shell=True)
-
 def ssh(arg,dry=False):
     if dry:
         print(f'{ssh_command} {arg}')
     else:
         return subprocess.getoutput(f'{ssh_command} {arg}')
-
-# quickly check if we actually have a functional ssh connection (might not be the case right after an update)
-checkmsg = ssh("/bin/true")
-if checkmsg != "":
-    print("ssh connection does not work, verify that you can manually ssh into your reMarkable. ssh itself commented the situation with:")
-    print(checkmsg)
-    ssh_connection.terminate()
-    sys.exit(255)
 
 
 def get_metadata_by_uuid(u):
@@ -65,26 +55,28 @@ def get_metadata_by_uuid(u):
 #
 #################################
 
-document_metadata = ssh(f'"ls -1 ~/.local/share/remarkable/xochitl/*.metadata"')
-metadata_uuids = set([pathlib.Path(d).stem for d in document_metadata.split('\n')])
+def cleanup_deleted():
 
-deleted_uuids = []
-limit = 10
-for u in tqdm.tqdm(metadata_uuids):
-    if get_metadata_by_uuid(u)['deleted']:
-        deleted_uuids.append(u)
+    document_metadata = ssh(f'"ls -1 ~/.local/share/remarkable/xochitl/*.metadata"')
+    metadata_uuids = set([pathlib.Path(d).stem for d in document_metadata.split('\n')])
 
-print(f'checking for deleted files - {limit}% done')
+    deleted_uuids = []
+    limit = 10
+    for u in tqdm.tqdm(metadata_uuids):
+        if get_metadata_by_uuid(u)['deleted']:
+            deleted_uuids.append(u)
 
-if len(deleted_uuids) == 0:
-    print('No deleted files found.')
-else:
+    print(f'checking for deleted files - {limit}% done')
 
-    decision = input(f'Clean up {len(deleted_uuids)} deleted files? [Y/n]')
-    if decision in ['', 'y', 'Y']:
-        for u in deleted_uuids:
-            ssh("rm -r ~/.local/share/remarkable/xochitl/{u}*", dry=args.dryrun)
+    if len(deleted_uuids) == 0:
+        print('No deleted files found.')
+    else:
+        decision = input(f'Clean up {len(deleted_uuids)} deleted files? [Y/n]')
+        if decision in ['', 'y', 'Y']:
+            for u in deleted_uuids:
+                ssh("rm -r ~/.local/share/remarkable/xochitl/{u}*", dry=args.dryrun)
 
+    return metadata_uuids
 
 
 #################################
@@ -93,36 +85,56 @@ else:
 #
 #################################
 
-all_document_ls = ssh(f'"ls -1 ~/.local/share/remarkable/xochitl"')
-all_document_files = [pathlib.Path(p) for p in all_document_ls.split('\n')]
-all_uuids = set([d.stem for d in all_document_files])
+def cleanup_orphaned(metadata_uuids):
+    all_document_ls = ssh(f'"ls -1 ~/.local/share/remarkable/xochitl"')
+    all_document_files = [pathlib.Path(p) for p in all_document_ls.split('\n')]
+    all_uuids = set([d.stem for d in all_document_files])
 
-orphaned_file_stems = all_uuids.difference(metadata_uuids)
+    orphaned_file_stems = all_uuids.difference(metadata_uuids)
 
-if len(orphaned_file_stems) > 0:
-    decision = input(f"Clear {len(orphaned_file_stems)} orphaned files that don't have metadata associated with them? [Y/n]")
-else:
-    decision = 'n'
+    if len(orphaned_file_stems) > 0:
+        decision = input(f"Clear {len(orphaned_file_stems)} orphaned files that don't have metadata associated with them? [Y/n]")
+    else:
+        decision = 'n'
 
-if decision in ['', 'y', 'Y']:
+    if decision in ['', 'y', 'Y']:
 
-    # now let's make sure all our orphaned nodes are actually unambiguous
-    orphan_deletion_candidates = []
-    for ofs in orphaned_file_stems:
-        # get all files that would match our ofs* deletion pattern later, and their stems
-        deletion_candidates = [d for d in all_document_files if d.name.startswith(ofs)]
-        orphan_stems = set([d.stem for d in deletion_candidates])
+        # now let's make sure all our orphaned nodes are actually unambiguous
+        orphan_deletion_candidates = []
+        for ofs in orphaned_file_stems:
+            # get all files that would match our ofs* deletion pattern later, and their stems
+            deletion_candidates = [d for d in all_document_files if d.name.startswith(ofs)]
+            orphan_stems = set([d.stem for d in deletion_candidates])
 
-        # if we didn't mismatch anything, i.e. having a file with stem 'a' and matching all uuids 'a*',
-        # we only have one orphan stem in the set. Otherwise we have an ambiguity and leave fixing to the user.
-        if len(orphan_stems) > 1:
-            print(f'~/.local/share/remarkable/xochitl/{ofs}* has no metadata, but matches more than one document or file. Ignoring this, you will have to check this manually.')
-        else:
-            orphan_deletion_candidates.append(ofs)
-
-
-    for of in orphan_deletion_candidates:
-        ssh(f'\'rm "/home/root/.local/share/remarkable/xochitl/{of}"*\'', dry=args.dryrun)
+            # if we didn't mismatch anything, i.e. having a file with stem 'a' and matching all uuids 'a*',
+            # we only have one orphan stem in the set. Otherwise we have an ambiguity and leave fixing to the user.
+            if len(orphan_stems) > 1:
+                print(f'~/.local/share/remarkable/xochitl/{ofs}* has no metadata, but matches more than one document or file. Ignoring this, you will have to check this manually.')
+            else:
+                orphan_deletion_candidates.append(ofs)
 
 
-ssh_connection.terminate()
+        for of in orphan_deletion_candidates:
+            ssh(f'\'rm "/home/root/.local/share/remarkable/xochitl/{of}"*\'', dry=args.dryrun)
+
+
+
+ssh_connection = None
+try:
+    ssh_connection = subprocess.Popen(f'{ssh_command} -o ConnectTimeout=1 -M -N -q ', shell=True)
+
+    # quickly check if we actually have a functional ssh connection (might not be the case right after an update)
+    checkmsg = ssh("/bin/true")
+    if checkmsg != "":
+        print("ssh connection does not work, verify that you can manually ssh into your reMarkable. ssh itself commented the situation with:")
+        print(checkmsg)
+        sys.exit(255)
+
+    metadata_uuids = cleanup_deleted()
+    cleanup_orphaned(metadata_uuids)
+
+finally:
+    if ssh_connection is not None:
+        ssh_connection.terminate()
+
+
